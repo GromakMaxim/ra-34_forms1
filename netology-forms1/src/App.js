e6b04e736849d8f91028298ffe411c8a6f14cdea
MaximGromak <maximgromak@gmail.com>
2022-03-24 20:50:56 +1000

    initial commit

---
+++ netology-forms1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+import '../src/css/main.css';
+import Widget from "./components/Widget";
+
+function App() {
+    return (
+        <div className="App">
+            <Widget/>
+        </div>
+    );
+}
+
+export default App;
---
+++ netology-forms1/src/components/Widget.js
@@ -0,0 +1,6 @@
+import MyForm from "./MyForm";
+import React from "react";
+
+export default function Widget(props) {
+    return <MyForm/>;
+}
